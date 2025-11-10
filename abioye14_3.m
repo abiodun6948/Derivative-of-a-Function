ddde3cce2e9e052b7884ef34d6937d7152f0d6d8
abiodun6948 <emmanuel.abioye@reedleycollege.edu>
2025-11-10 22:06:18 +0000

    last practice for the day

--- abioye14_3.m
+++ abioye14_3.m
@@ -1,1 +1,13 @@
 
+%Integral and derivative of a Polynomial
+p = input("Enter the coefficients of a " + ...
+    "cubic polynomial, as a matrix[..]\n"); 
+y0 = input("Enter the integration constant for that function.\n");
+pDer = polyder(p);
+pint = polyint(p,y0);
+%Report the output
+fprintf("The derivative of the function is %gx^2+%gx+%g.\n",pDer)
+fprintf("The integral of the function is %.2gx^4+%.2gx^3+%.2gx^2+%g.2x+%g.\n",pint)
+
+
+
